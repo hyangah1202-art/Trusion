66fce03adc0001b65f62ffaa8390295e0238ccb3
hyangah1202-art <hyangah1202@gmail.com>
2025-09-28 11:59:01 +0900

    Delete assets directory

--- assets/js/menu.js
+++
@@ -1,9 +0,0 @@
-
-// Simple active link helper
-(function(){
-  const path = location.pathname.split('/').pop() || 'index.html';
-  document.querySelectorAll('.menu .nav-link').forEach(a=>{
-    const href = a.getAttribute('href');
-    if (href && href.indexOf(path) !== -1) a.classList.add('active');
-  });
-})();
